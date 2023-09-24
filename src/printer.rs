a8ffe655f8b4719a9272294cd85558c3661060ad
nerix <nerixdev@outlook.de>
2023-09-24 14:00:16 +0200

    feat: add `-o` to output to a file (#45)

--- src/printer.rs
+++ src/printer.rs
@@ -1,4 +1,8 @@
-use std::io::{StdoutLock, Write};
+use std::{
+    ffi::OsStr,
+    fs,
+    io::{self, Write},
+};
 
 use anyhow::Context;
 use widestring::U16Str;
@@ -6,19 +10,28 @@ use windows::Win32::{Foundation::HANDLE, System::Diagnostics::Debug::OUTPUT_DEBU
 
 use crate::strings::read_string_into;
 
-pub struct Printer<'a> {
-    stdout: StdoutLock<'a>,
+pub struct Printer {
+    writer: Box<dyn io::Write>,
     buffer: Vec<u16>,
     process_handle: HANDLE,
 }
 
-impl<'a> Printer<'a> {
-    pub fn new(process_handle: HANDLE) -> Self {
-        Self {
-            stdout: std::io::stdout().lock(),
+impl Printer {
+    pub fn new(process_handle: HANDLE, file: Option<&OsStr>) -> io::Result<Self> {
+        Ok(Self {
+            writer: match file {
+                Some(f) => Box::new(
+                    fs::OpenOptions::new()
+                        .write(true)
+                        .truncate(true)
+                        .create(true)
+                        .open(f)?,
+                ),
+                None => Box::new(std::io::stdout().lock()),
+            },
             buffer: Vec::new(),
             process_handle,
-        }
+        })
     }
 
     pub fn read_string(&mut self, info: OUTPUT_DEBUG_STRING_INFO) -> anyhow::Result<()> {
@@ -28,8 +41,26 @@ impl<'a> Printer<'a> {
         };
 
         let line = U16Str::from_slice(&self.buffer);
-        write!(self.stdout, "{}", line.display()).ok();
+        write!(self.writer, "{}", OutputDisplay { s: line }).ok();
 
+        Ok(())
+    }
+}
+
+struct OutputDisplay<'a> {
+    s: &'a U16Str,
+}
+
+// Adapted from widestring::Display, but we're ignoring \0 here
+impl<'a> std::fmt::Display for OutputDisplay<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use std::fmt::Write;
+
+        for c in widestring::decode_utf16_lossy(self.s.as_slice().iter().copied()) {
+            if c != '\0' {
+                f.write_char(c)?;
+            }
+        }
         Ok(())
     }
 }
